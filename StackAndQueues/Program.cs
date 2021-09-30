a006d09c17642ae98fb3ac45af89196bd84a1085
charasullia <charanrajr713@gmail.com>
2021-09-30 21:14:40 +0530

    [charansullia]added:stack operation using LinkedList

---
+++ StackAndQueues/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StackAndQueues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StackAndQueue list = new StackAndQueue();
+            list.Add(70);
+            list.Add(30);
+            list.Add(56);
+            list.Display();
+
+        }
+    }
+}
---
+++ StackAndQueues/StackAndQueue.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackAndQueues
+{
+    class StackAndQueue
+    {
+        private Node top;
+        public StackAndQueue()
+        {
+            this.top = null;
+        }
+        internal void Add(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+                node.next = null;
+            else
+                node.next = this.top;
+            this.top = node;
+            Console.WriteLine("{0} Added to stack ", value);
+        }
+        public void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
